88ee40ef313830e11ad006df0376ce928c5392fe
Sarthak <sarthakj1195@gmail.com>
2025-07-11 23:15:58 +0530

    new file:   Apna College/2/Scan.class 	new file:   Apna College/2/Scan.java 	new file:   Apna College/2/Sum.class 	new file:   Apna College/2/Sum.java

---
+++ Apna College/2/Scan.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Scan {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("hello");
+        String name= sc.nextLine();
+        System.out.println(name);
+    }
+}
---
+++ Apna College/2/Sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Values");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Sum is "+(a+b));
+
+    }
+    
+}
