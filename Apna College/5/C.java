d0c5086bdbb633432979e8f5d19a5cc53be898eb
Sarthak <sarthakj1195@gmail.com>
2025-07-14 22:28:44 +0530

    new file:   Apna College/5/C.class 	new file:   Apna College/5/C.java 	new file:   Apna College/5/D.class 	new file:   Apna College/5/D.java 	new file:   Apna College/5/E.java

---
+++ Apna College/5/C.java
@@ -0,0 +1,11 @@
+public class C {
+    public static void main(String[] args) {
+        int i=0,j=0;
+        for(i=0;i<=3;i++){
+            for(j=0;j<=i;j++){
+                System.out.print("*");       
+        }
+        System.out.println("");
+    }
+}
+}
---
+++ Apna College/5/D.java
@@ -0,0 +1,11 @@
+public class D {
+    public static void main(String[] args) {
+        int i=0,j=0;
+        for(i=0;i<=3;i++){
+            for(j=0;j+i<4;j++){
+                System.out.print("*");       
+        }
+        System.out.println("");
+    }
+}
+}
