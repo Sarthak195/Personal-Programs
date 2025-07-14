b9f3ebcc349fb522435902e5446c5d4b95e86bee
Sarthak <sarthakj1195@gmail.com>
2025-07-14 23:09:05 +0530

    new file:   Apna College/5/F.class 	new file:   Apna College/5/F.java

---
+++ Apna College/5/F.java
@@ -0,0 +1,14 @@
+public class F {
+    public static void main(String[] args) {
+        int i=0,j=0,k=0;
+        for(i=0;i<=3;i++){
+            for(j=0;i+j <=3;j++){
+                System.out.print(" ");  
+            }
+            for(k=0;k<=i;k++){
+                System.out.print("*");
+            }
+        System.out.println("");
+        }
+    }
+}
