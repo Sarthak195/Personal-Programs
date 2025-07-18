7d42e0193ace1382472f82b4c2cce306a5cc35a1
Sarthak <sarthakj1195@gmail.com>
2025-07-18 23:18:13 +0530

    new file:   Apna College/6/Sum.class 	new file:   Apna College/6/Sum.java

---
+++ Apna College/6/Sum.java
@@ -0,0 +1,13 @@
+
+
+public class Sum {
+    public static void print(int a, int b){
+        int sum =a+b;
+        System.out.println(sum);
+    }
+    public static void main(String[] args) {
+        print(5,6);
+       
+    }
+    
+}
