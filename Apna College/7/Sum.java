8a735f44afcd30dd58ee9e4f77e460b5908c8b19
Sarthak <sarthakj1195@gmail.com>
2025-07-19 21:36:37 +0530

    new file:   Apna College/7/Sum.class 	modified:   Apna College/7/Sum.java

--- Apna College/7/Sum.java
+++ Apna College/7/Sum.java
@@ -1,12 +1,16 @@
-
+import java.util.Scanner;
 
 public class Sum {
     public static void print(int a, int b){
         int sum =a+b;
-        System.out.println(sum);
+        System.out.println("Your Sum is 5"+sum);
     }
     public static void main(String[] args) {
-        print(5,6);
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter values:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        print(a,b);
        
     }
     
