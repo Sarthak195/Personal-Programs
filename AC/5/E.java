bd9764b1329029b2b60e97bf55f33c46ac2af569
Sarthak195 <sarthakj1195@gmail.com>
2025-09-01 22:42:07 +0530

    modified:   AC/5/E.java     	modified:   Python/py1.py 	new file:   Python/py2.py

--- AC/5/E.java
+++ AC/5/E.java
@@ -8,7 +8,7 @@ public class E {
     public static void main(String[] args) {
         int i,j,k;
         for(i=0;i<=3;i++){
-            for(j=0;i+j <=3;j++){
+            for(j=0;i+j<=3;j++){
                 System.out.print(" ");  
             }
             for(k=0;k<=i;k++){
--- Python/py1.py
+++ Python/py1.py
@@ -1,3 +1,2 @@
-rows = 5
-for i in range(1,rows,1):
+for i in range(1,8,1):
     print('*' * i)
