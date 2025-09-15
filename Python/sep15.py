ed0135420b735648079f8cf3fb2b655c84ece5d2
Sarthak195 <sarthakj1195@gmail.com>
2025-09-15 16:43:44 +0530

    new file:   Python/sep15.py

---
+++ Python/sep15.py
@@ -0,0 +1,22 @@
+#Capitalize
+str="    Hello, hi, bye    "
+str1=str.capitalize()
+print (str1)
+#title
+str2=str.title()
+print (str2)
+#find
+str3=str.find('bye')
+print (str3)
+#index
+str4=str.index('bye')
+print (str4)
+#lstrip
+str5=str.lstrip()
+print (str5)
+#lstrip
+str6=str.rstrip()
+print (str6)
+#strip
+str7=str.strip()
+print (str7)
