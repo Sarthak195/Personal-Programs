7a8a2086942357dceb6ea7a783a6d35f079f5166
Sarthak195 <sarthakj1195@gmail.com>
2025-11-12 16:37:08 +0530

    modified:   Python/palindrome.py

--- Python/palindrome.py
+++ Python/palindrome.py
@@ -1,4 +1,6 @@
-num=int(input("Enter number to check:"))
-res= num
+num=input("Enter number to check:")
+res= num[::-1]
 if res==num:
     print(num,"is a palindrome")
+else:
+    print(num," is not a palindrome")
