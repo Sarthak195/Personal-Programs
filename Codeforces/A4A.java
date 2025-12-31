deb079366ee06ea01fa14eca5f2baeb980ba8d10
Sarthak195 <sarthakj1195@gmail.com>
2026-01-01 02:41:41 +0530

    modified:   Codeforces/A4A.class 	modified:   Codeforces/A4A.java 	renamed:    Codeforces/71A -> Codeforces/A71A.java

--- Codeforces/A4A.java
+++ Codeforces/A4A.java
@@ -1,13 +1,9 @@
-public class A4A
-{
+import java.util.*;
+
+public class A4A {
     public static void main(String[] args) {
-        java.util.Scanner sc = new java.util.Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
-        
-        if (w % 2 == 0 && w > 2) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+        System.out.println(w > 2 && w % 2 == 0 ? "YES" : "NO");
     }
 }
--- Codeforces/A71A.java
+++ Codeforces/A71A.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class A71A {
     static FastScanner fs = new FastScanner();
     static PrintWriter out = new PrintWriter(System.out);
 
