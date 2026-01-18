68c8c1e36b2aab95dc985352ae48ff802e784653
Sarthak195 <sarthakj1195@gmail.com>
2026-01-18 18:33:13 +0530

    modified:   Codeforces/A1296B.java

--- Codeforces/A1296B.java
+++ Codeforces/A1296B.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-
 public class A1296B {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
         while (t-- > 0) {
             long s = sc.nextLong();
             long ans = s + (s - 1) / 9;
             System.out.println(ans);
         }
+        sc.close(); 
     }
 }
