fe704c1fea80b321f1c4d04d8ee520a078504408
Sarthak195 <sarthakj1195@gmail.com>
2026-01-24 17:18:24 +0530

    new file:   Codeforces/A1374B.class 	new file:   Codeforces/A1374B.java 	new file:   Codeforces/A1878C.class new file:   Codeforces/A1878C.java 	new file:   Codeforces/tempCodeRunnerFile.java

---
+++ Codeforces/A1878C.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class A1878C {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {long n = sc.nextLong();
+            long k = sc.nextLong();long x = sc.nextLong();
+            long minSum = k * (k + 1) / 2;
+            long maxSum = k * n - k * (k - 1) / 2;
+
+            if (x >= minSum && x <= maxSum) {
+                System.out.println("YES");} else {
+           System.out.println("NO");
+}    }
+}
+}
