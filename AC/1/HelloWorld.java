508c70cb71165d8ae3ff029e0b05358ed97e3d7a
Sarthak195 <sarthakj1195@gmail.com>
2026-05-27 16:00:27 +0530

    modified:   AC/1/HelloWorld.class 	modified:   AC/1/HelloWorld.java

--- AC/1/HelloWorld.java
+++ AC/1/HelloWorld.java
@@ -1,6 +1,14 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] num={10,20,30,40,50};
+        inc(num, 5);
+        System.out.println();
+        inc(num, -10);
+        }
+    public static void inc (int[] num, int diff){
+        for(int i=0;i<num.length;i++){
+            num[i]+=diff;
+            System.out.print(num[i]+" ");
+            }
+        }
     }
-    
-}
